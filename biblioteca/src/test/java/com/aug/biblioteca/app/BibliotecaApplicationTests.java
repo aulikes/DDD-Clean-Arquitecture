72caaeda3af3eb291233ad8fcdf1d22e01f7df17
aulikes <aulikes@hotmail.com>
2025-05-24 20:59:18 -0500

    Add Biblioteca

---
+++ biblioteca/src/test/java/com/aug/biblioteca/app/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aug.biblioteca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
